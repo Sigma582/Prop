cba3f9e0b8dce2c1e9f513832751f05d17d6e41b
author <26780385+Sigma582@users.noreply.github.com>
2021-11-14 01:17:57 +0100

    renamed a parameter for clarity

--- Sigma.Prop/Sigma.Prop/IAccessor.cs
+++ Sigma.Prop/Sigma.Prop/IAccessor.cs
@@ -5,8 +5,8 @@ namespace Sigma.Prop
 {
     public interface IAccessor
     {
-        object Get(object target, string property);
-        TProperty Get<TProperty>(object target, string property);
+        object Get(object target, string propertyName);
+        TProperty Get<TProperty>(object target, string propertyName);
         void Set(object target, string propertyName, object value);
     }
 }
